e695dfc23f9a4960c8bdd3e53fce2a088bdc2255
berkaybyr <berkaybyr@icloud.com>
2023-07-12 21:14:48 +0300

    new patch version release

--- src/EasMe.Test/Program.cs
+++ src/EasMe.Test/Program.cs
@@ -1,15 +1,6 @@
 ﻿using System.Net;
 using EasMe;
 
-Console.WriteLine("Hello World!");
-var ipAddd = GetHostIpAddress();
 
+Console.ReadKey();
 
-Console.ReadLine();
-static string GetHostIpAddress() {
-  var ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
-  var ipAddress = ipHostInfo.AddressList.Select(x => x.ToString())
-                            .Where(x => !x.StartsWith("192.168.") && !x.StartsWith("::"))
-                            .ToList();
-  return ipAddress.FirstOrDefault();
-}
